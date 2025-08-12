217b80766482f4a2eae5361e54eb197cf21fd7b8
MARCELO VIEIRA DA SILVA <25132416@CFP127ALUNO.LOCAL>
2025-08-12 09:21:49 -0300

    alterando nome de tag

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ function criarImagem(srcImagem){
         const galeria = document.getElementById('galeria')
 
         const imagem = document.createElement('img')
-        const legenda = document.createElement('leg')
+        const legenda = document.createElement('span')
 
         imagem.src = srcImagem.url
         legenda.textContent = srcImagem.nome
